b40dca3da794641d6167cac7f8e12b8fc835c51f
GuntojuNaveen <navneethi198@gmail.com>
2026-05-11 00:15:25 +0530

    Delete application-code/DbConfig.js

--- application-code/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'database-1.cdi22w0cwkp5.ap-south-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'admin12345',
-    DB_DATABASE : 'webappdb'
-});
